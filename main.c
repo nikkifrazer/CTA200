2387fd9c77292d88b12654c1d0af4257f0f1005f
Nikki F <n.frazer@mail.utoronto.ca>
2020-05-06 17:03:34 -0400

    Updated code: changed the return to include 1.5 instead of 1

--- main.c
+++ main.c
@@ -3,10 +3,13 @@
 
 
 int main() {
-    double arr[] = {1.0, 2.0, 3.0, 4.0};
+    double arr[] = {1.5, 2.0, 3.0, 4.0};
 
     double result = average(4, arr);
 
-    printf("The average of 1, 2, 3 and 4 is: %.4f\n", result);
+    printf("The average of 1.5, 2, 3 and 4 is: %.4f\n", result);
     return 0;    
 }
+
+/* Changed 1 to 1.5, and averaged the four numbers */
+
