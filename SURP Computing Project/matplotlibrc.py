bdaa26efbaf0a4a639a10239eb404a499c516d94
nikkifrazer <64741511+nikkifrazer@users.noreply.github.com>
2020-05-12 10:54:18 -0400

    Add files via upload

---
+++ SURP Computing Project/matplotlibrc.py
@@ -0,0 +1,8 @@
+# This is a sample matplotlib configuration file
+### FIGURE
+
+figure.dpi       : 150      # figure dots per inch
+
+### SAVING FIGURES
+
+savefig.dpi      : 150      # figure dots per inch
